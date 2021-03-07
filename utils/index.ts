22071624e0bacc4d2f593c5d1e6cce51abf8939e
gtg7784 <tae.gun7784@gmail.com>
2021-03-07 15:49:41 +0900

    fix: section mobile height

---
+++ utils/index.ts
@@ -0,0 +1,8 @@
+export const isMobile = () => {
+	const UserAgent = navigator.userAgent;
+	if (UserAgent.match(/iPhone|iPod|Android|Windows CE|BlackBerry|Symbian|Windows Phone|webOS|Opera Mini|Opera Mobi|POLARIS|IEMobile|lgtelecom|nokia|SonyEricsson/i) != null || UserAgent.match(/LG|SAMSUNG|Samsung/) != null) {
+		return true;
+	} else {
+		return false;
+	}
+}
